b4bcdaa53ef0876fa59a5151e764905bfa2c94e5
Your Name <you@example.com>
2021-07-02 14:12:25 +0000

    init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,50 @@
 class PostsController < ApplicationController
   def index
       #@をつける事でテンプレートに直接渡せる
-     @posts=Post.all
+    @posts=Post.all.order(created_at: :desc)
   end
   
   def show
-     @post=Post.find_by(id:params[:id])
+    @post=Post.find_by(id: params[:id])
   end
   
   def new
+  end
+  
+  
+  #コメント入れるときは全角要注意！
+  def create
+    #データベースへの保存
+    @post=Post.new(content:params[:content])
+    @post.save
+    #テンプレートがないのでリダイレクト
+    redirect_to("/posts/index")#対応ビューがないのでリダイレクトさせる
+  end
+  #編集アクション
+  def edit
+      @post=Post.find_by(id: params[:id])
+  end
+  
+  #更新アクション
+  def update
+      key=params[:id]
+      @post=Post.find_by(id: key)
+      @post.content=params[:content]
+      @post.save
+      
+     
+      redirect_to controller: :posts ,action: :edit, id: key
+  end
+  
+  def verification
+      @post=Post.find_by(id: params[:id])
+  end
+  
+  #削除アクション
+  def destory
+      key=params[:id]
+      @post=Post.find_by(id: key)
+      @post.destroy
+      redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
 #ルーティングは送信されたurLに対するコントローラの対応表
 #get URL => コントローラ#アクション
-  get '/' => "home#top"
-  get 'about' => "home#about"
+ 
+    get "/" => "home#top"
+    get "about" => "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #postコントローラ対応表
-  get 'posts/index' => 'posts#index' 
+    get "posts/index" => 'posts#index' 
   #idを利用した動的ルーティング
-  get 'posts/new' => 'posts#new'
-  get 'posts/:id' => 'posts#show'
+    get "posts/new" => 'posts#new'
+    get "posts/:id" => 'posts#show'
+    post "posts/create" => 'posts#create'
+    
+    #編集アクション
+    get 'posts/:id/edit' => "posts#edit"
+    post 'posts/:id/update' => "posts#update"
+    get "posts/:id/verification"=>"posts#verification"
+    post 'posts/:id/destory' => "posts#destory"
   
-  #新規投稿作成画面
- 
+#ホーム画面
 end
