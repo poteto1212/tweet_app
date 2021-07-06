45cb467246bd9008f373657074ee01e8551ace24
Your Name <you@example.com>
2021-07-06 01:00:43 +0000

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,22 @@
 class UsersController < ApplicationController
   def index
-      @users=User.all
+    @users=User.all
   end
 
   def show
     @user=User.find_by(id: params[:id])
   end
   def new
-
+    @user=User.new 
   end
 
   def create
     @user=User.new(name: params[:name],email: params[:email])
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    if @user.save
+      flash[:notice]="ユーザー登録が完了しました。"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
 end
