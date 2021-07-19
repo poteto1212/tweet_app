bffe7ae1260127dbca34b2f49cdecf022acedc7b
Your Name <you@example.com>
2021-07-19 07:17:09 +0000

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
     validates :password,{presence: true}
+
+
+    #ユーザー別投稿機能を作成する為のモデルメソッド
+
+    def posts
+        return Post.where(user_id: self.id)
+    end
 end
